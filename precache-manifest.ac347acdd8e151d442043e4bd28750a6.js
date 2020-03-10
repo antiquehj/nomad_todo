482efa1ae2032bce4d7fcdb1a0e42e90c5a3b7a7
antiquehj <hyunjooj@naver.com>
2020-03-11 00:47:02 +0900

    Updates

---
+++ precache-manifest.ac347acdd8e151d442043e4bd28750a6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe55049babdb0e508daf013156429333",
+    "url": "/nomad_todo/index.html"
+  },
+  {
+    "revision": "e8feef33336eef7fafa3",
+    "url": "/nomad_todo/static/css/main.4faf5546.chunk.css"
+  },
+  {
+    "revision": "61e06145e536d2b2bb76",
+    "url": "/nomad_todo/static/js/2.99c04c95.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/nomad_todo/static/js/2.99c04c95.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e8feef33336eef7fafa3",
+    "url": "/nomad_todo/static/js/main.b7c6347d.chunk.js"
+  },
+  {
+    "revision": "574bfac40a57211a1295",
+    "url": "/nomad_todo/static/js/runtime-main.0694e5d7.js"
+  }
+]);
